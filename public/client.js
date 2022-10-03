d72d5dc679548602d100820f6faab00e2bb3384c
snishant011 <shresthanishant011@gmail.com>
2022-10-03 16:53:41 +0545

    improved frontend code and bugfixes
    messages are animated, removed InnerHTMl code which can prevent XSS attack

--- public/client.js
+++ public/client.js
@@ -1,53 +1,63 @@
 const socket = io()
-let name;
+let username;
 let textarea = document.querySelector('#textarea')
 let messageArea = document.querySelector('.message__area')
-do {
-    name = prompt('Please enter your name: ')
-} while(!name)
+username = prompt('Please enter your name: ')
+
+if (!username) {
+  fetch("https://randomuser.me/api/?nat=us&randomapi").then((res) => res.json()).then(data => {
+    username = data.results[0].login.username;
+    console.log(data)
+  }).catch((err) => {
+    console.log(err);
+    username = "randomuser";
+  }).catch((err) => {
+    console.log(err);
+    username = "randomuser";
+  })
+}
 
 textarea.addEventListener('keyup', (e) => {
-    if(e.key === 'Enter') {
-        sendMessage(e.target.value)
-    }
+  if (e.key === 'Enter') {
+    sendMessage(e.target.value)
+  }
 })
 
 function sendMessage(message) {
-    let msg = {
-        user: name,
-        message: message.trim()
-    }
-    // Append 
-    appendMessage(msg, 'outgoing')
-    textarea.value = ''
-    scrollToBottom()
+  let msg = {
+    user: username,
+    message: message.trim()
+  }
+  // Append 
+  appendMessage(msg, 'outgoing')
+  textarea.value = ''
+  scrollToBottom()
 
-    // Send to server 
-    socket.emit('message', msg)
+  // Send to server 
+  socket.emit('message', msg)
 
 }
 
 function appendMessage(msg, type) {
-    let mainDiv = document.createElement('div')
-    let className = type
-    mainDiv.classList.add(className, 'message')
-
-    let markup = `
-        <h4>${msg.user}</h4>
-        <p>${msg.message}</p>
-    `
-    mainDiv.innerHTML = markup
-    messageArea.appendChild(mainDiv)
+  let mainDiv = document.createElement('div')
+  let className = type
+  mainDiv.classList.add(className, 'message')
+  const userTitle = document.createElement('h4');
+  userTitle.innerText = msg.user;
+  const userMessage = document.createElement('p');
+  userMessage.innerText = msg.message;
+  mainDiv.append(userTitle, userMessage);
+  messageArea.appendChild(mainDiv);
 }
 
 // Recieve messages 
 socket.on('message', (msg) => {
-    appendMessage(msg, 'incoming')
-    scrollToBottom()
+  appendMessage(msg, 'incoming')
+  scrollToBottom()
 })
 
 function scrollToBottom() {
-    messageArea.scrollTop = messageArea.scrollHeight
+  messageArea.scrollTop = messageArea.scrollHeight
 }
 
 
